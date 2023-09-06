2c0cee6c5ea2eca67f8bae2303ad644d4eb024cf
Newtron21 <annacpnewton@gmail.com>
2023-09-05 20:43:52 -0600

    adding HTML

---
+++ Assets/JS/script.js
@@ -0,0 +1,9 @@
+
+//when someone press search button take city and put into API to get lat and long variables
+//need to do a weather call fetch api
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={125ec5dfb3845d6eee989db1b778bdc8}
+//saved searches down below for past searches
+//saved searches need to be clickable to go back to previous searches
+//from fetch call I need to get variables for City name, date, icon for weather, temp, wind, humidity 
+//from fetch call I need to do that again but each one date +1 and all the same information. 
+
